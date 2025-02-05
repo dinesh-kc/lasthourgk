73c7188dcd18c1e783bab7d7b978e75714293b9f
dinesh-kc <dineshkc.mail@gmail.com>
2025-02-05 16:54:20 +0545

    fjdlf

--- aarthik.js
+++ aarthik.js
@@ -1,7 +1,7 @@
 
-// angular.module('mcqApp', [])
+angular.module('mcqApp', [])
 
-mcqApp.controller('mcqController', function($scope) {
+  .controller('mcqController', function($scope) {
     // Replace the random question data with your provided JSON data
     $scope.questions = [
 
